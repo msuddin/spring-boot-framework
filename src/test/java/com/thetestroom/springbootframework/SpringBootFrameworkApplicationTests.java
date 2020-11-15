77223ecee9bc4f8a86c75cf8149d8ba16a5dddf5
Mohammed Shahin Uddin <mo@thetestroom.com>
2020-11-15 11:23:16 +0000

    Initial commit

---
+++ src/test/java/com/thetestroom/springbootframework/SpringBootFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thetestroom.springbootframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
